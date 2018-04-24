aba6440b152daffaebf45b0977a3eb6002fcb152
Manakov Alexander <alexander.s.manakov@gmail.com>
2018-04-24 11:20:52 +0700

    Photos refactoring

--- modules/photos.js
+++ modules/photos.js
@@ -2,41 +2,35 @@ const express = require('express')
 const router = express.Router()
 const { addPhoto, changePhoto, getUserPhotos, getAllPhotos } = require('../sql/photos')
 const upload = require('../middlewares/upload')
+const authorize = require('../middlewares/authorize')
 
-router.post('', upload.single('file'), (req, res, next) => {
-  addPhoto(req.session.id, 'http://localhost:3000/' + req.file.filename, req.body.title, req.body.description)
+router.post('', authorize, upload.single('file'), (req, res, next) => {
+  addPhoto(req.currentUser.id, 'http://localhost:3000/' + req.file.filename, req.body.title, req.body.description)
     .then(photo => res.json(photo))
     .catch(error => next(error))
 });
 
-router.put('/:photoId', (req, res, next) => {
-  changePhoto(req.body.id, req.body.title, req.body.description, req.session.id)
+router.put('/:photoId', authorize, (req, res, next) => {
+  changePhoto(req.body.id, req.body.title, req.body.description, req.currentUser.id)
     .then(photo => res.json(photo))
     .catch(error => next(error))
 });
 
-router.delete('/:photoId', (req, res, next) => {
-  deletePhotoById(req.params.photoId, req.session.id)
-    .then(() => {
-      res.send();
-      res.end();
-    });
+router.delete('/:photoId', authorize, (req, res, next) => {
+  deletePhotoById(req.params.photoId, req.currentUser.id)
+    .then(() => res.json());
 });
 
-router.get('/getUserPhotos', (req, res, next) => {
-  getUserPhotos(req.session.id)
-    .then(photos => {
-      res.send(photos);
-      res.end();
-    });
+router.get('/getUserPhotos', authorize, (req, res, next) => {
+  getUserPhotos(req.currentUser.id)
+    .then(photos => res.json(photos))
+    .catch(error => next(error))
 });
 
-router.get('/getAllPhotos', (req, res, next) => {
-  getAllPhotos(req.session.id)
-    .then(photos => {
-      res.send(photos);
-      res.end();
-    });
+router.get('/getAllPhotos', authorize, (req, res, next) => {
+  getAllPhotos(req.currentUser.id)
+    .then(photos => res.json(photos))
+    .catch(error => next(error))
 });
 
 module.exports = router
